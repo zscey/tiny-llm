35772e9e2b4957b2146fc92a5f379d49a2bcf71e
haol <haol>
2025-12-11 01:50:39 +0800

    refactor: remove get_ptr() in Buffer

--- tiny_llm/device_managers/buffer.hpp
+++ tiny_llm/device_managers/buffer.hpp
@@ -52,8 +52,6 @@ public:
 
   ~Buffer() noexcept;
 
-  auto get_ptr() -> void * { return ptr_; }
-
   [[nodiscard]] auto get_ptr() const -> const void * { return ptr_; }
 
   [[nodiscard]] auto get_size() const -> std::size_t { return size_; }
