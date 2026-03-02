0b45bbfadabac21ae3e9901d6ee8e0454ece5289
zscey <zscey.haol@gmail.com>
2026-03-02 22:56:44 +0800

    feat: embedding impl

--- tiny_llm/ops/cuda/embedding.hpp
+++ tiny_llm/ops/cuda/embedding.hpp
@@ -4,9 +4,13 @@
 
 namespace tiny_llm::cuda {
 /**
- * src: [n]
- * emb_weight: [m, dim]
- * dst [n, dim]
+ * @brief Embedding
+ *
+ * @param src View as a shape [n].
+ * @param emb_weights View as a shape [m, dim].
+ * @param dst View as a shape [n, dim].
+ * @param dim
+ * @param element_size Equal to n.
  */
 void embedding(const uint32_t *src, const float *emb_weights, float *dst,
                uint32_t dim, size_t element_size);
