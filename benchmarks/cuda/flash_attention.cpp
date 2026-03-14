73dd7d534c41ce2d762497df3b4afcf8421aafaf
zscey <zscey.haol@gmail.com>
2026-03-15 00:03:02 +0800

    feat: Impl casual flash attention

--- benchmarks/cuda/flash_attention.cpp
+++ benchmarks/cuda/flash_attention.cpp
@@ -5,7 +5,7 @@
 
 namespace tiny_llm {
 namespace {
-void bm_flash_attn(benchmark::State &state) {
+void bm_flash_attn_no_mask(benchmark::State &state) {
   int64_t batch = 2;
   int64_t q_head = 8;
   int64_t kv_head = 4;
@@ -23,13 +23,43 @@ void bm_flash_attn(benchmark::State &state) {
   for (auto _ : state) {
     cuda::flash_attn(query.data<float>(), key.data<float>(),
                      value.data<float>(), dst.data<float>(), batch,
-                     state.range(0), state.range(0), dim, q_head, kv_head);
+                     state.range(0), state.range(0), dim, q_head, kv_head,
+                     cuda::AttentionType::kNoMask);
+    ThreadCudaContexts::Synchronize();
+  }
+}
+
+void bm_flash_attn_causal_mask(benchmark::State &state) {
+  int64_t batch = 2;
+  int64_t q_head = 8;
+  int64_t kv_head = 4;
+  uint32_t dim = 64;
+  Tensor query({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+               {batch, state.range(0), q_head * dim}, true);
+  Tensor key({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+             {batch, state.range(0), kv_head * dim}, true);
+  Tensor value({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+               {batch, state.range(0), kv_head * dim}, true);
+  Tensor dst({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+             {batch, state.range(0), q_head * dim}, true);
+  ThreadCudaContexts::Synchronize();
+
+  for (auto _ : state) {
+    cuda::flash_attn(query.data<float>(), key.data<float>(),
+                     value.data<float>(), dst.data<float>(), batch,
+                     state.range(0), state.range(0), dim, q_head, kv_head,
+                     cuda::AttentionType::kCausalMask);
     ThreadCudaContexts::Synchronize();
   }
 }
 } // namespace
 
-BENCHMARK(bm_flash_attn)
+BENCHMARK(bm_flash_attn_no_mask)
+    ->Ranges({{1, 9192}})
+    ->RangeMultiplier(8)
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK(bm_flash_attn_causal_mask)
     ->Ranges({{1, 9192}})
     ->RangeMultiplier(8)
     ->Unit(benchmark::kMillisecond);
--- tests/ops/cuda/test_flash_attention.cpp
+++ tests/ops/cuda/test_flash_attention.cpp
@@ -5,7 +5,7 @@
 #include "gtest/gtest.h"
 
 namespace tiny_llm {
-TEST(CudaOps, Func) {
+TEST(CudaOps, FlashAttnNoMask) {
   uint32_t batch = 2;
   uint32_t q_length = 107;
   uint32_t q_head = 4;
@@ -48,7 +48,8 @@ TEST(CudaOps, Func) {
                {batch, q_length, static_cast<int64_t>(q_head * dim)});
     cuda::flash_attn(query.data<float>(), key.data<float>(),
                      value.data<float>(), dst.data<float>(), batch, q_length,
-                     kv_length, dim, q_head, kv_head);
+                     kv_length, dim, q_head, kv_head,
+                     cuda::AttentionType::kNoMask);
     dst = dst.to({.type = DeviceType::kCpu, .id = 0});
     ThreadCudaContexts::Synchronize();
 
@@ -65,4 +66,64 @@ TEST(CudaOps, Func) {
   }
 }
 
+TEST(CudaOps, FlashAttnCausalMask) {
+  uint32_t batch = 2;
+  uint32_t q_length = 107;
+  uint32_t q_head = 4;
+  uint32_t kv_length = q_length;
+  uint32_t kv_head = 2;
+  uint32_t dim = 61;
+
+  Tensor query({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {batch, q_length, static_cast<int64_t>(q_head * dim)});
+  auto *query_ptr = query.data<float>();
+  for (size_t i = 0,
+              i_end = static_cast<size_t>(batch) * q_length * q_head * dim;
+       i < i_end; ++i) {
+    query_ptr[i] = static_cast<float>(i % 100) / 100;
+  }
+  query = query.to({.type = DeviceType::kCuda, .id = 0});
+
+  Tensor key({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+             {batch, kv_length, static_cast<int64_t>(kv_head * dim)});
+  auto *key_ptr = key.data<float>();
+  for (size_t i = 0,
+              i_end = static_cast<size_t>(batch) * kv_length * kv_head * dim;
+       i < i_end; ++i) {
+    key_ptr[i] = static_cast<float>((i + 1) % 100) / 100;
+  }
+  key = key.to({.type = DeviceType::kCuda, .id = 0});
+
+  Tensor value({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {batch, kv_length, static_cast<int64_t>(kv_head * dim)});
+  auto *value_ptr = value.data<float>();
+  for (size_t i = 0,
+              i_end = static_cast<size_t>(batch) * kv_length * kv_head * dim;
+       i < i_end; ++i) {
+    value_ptr[i] = static_cast<float>((i + 2) % 100) / 100;
+  }
+  value = value.to({.type = DeviceType::kCuda, .id = 0});
+
+  {
+    Tensor dst({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+               {batch, q_length, static_cast<int64_t>(q_head * dim)});
+    cuda::flash_attn(query.data<float>(), key.data<float>(),
+                     value.data<float>(), dst.data<float>(), batch, q_length,
+                     kv_length, dim, q_head, kv_head,
+                     cuda::AttentionType::kCausalMask);
+    dst = dst.to({.type = DeviceType::kCpu, .id = 0});
+    ThreadCudaContexts::Synchronize();
+
+    SafeTensorWeightManager wm(utils::BazelRunfile::RLocation(
+        "tiny_llm/tests/datas/casual_attn_res_b2l107q4k2d61.safetensors"));
+    const auto *dst_ptr = dst.data<float>();
+    const auto *target_ptr =
+        reinterpret_cast<const float *>(wm.get_tensor("res").data);
+    for (size_t i = 0,
+                i_end = static_cast<size_t>(batch) * q_length * q_head * dim;
+         i < i_end; ++i) {
+      EXPECT_TRUE(std::abs(dst_ptr[i] - target_ptr[i]) < 1e-5F);
+    }
+  }
+}
 } // namespace tiny_llm
