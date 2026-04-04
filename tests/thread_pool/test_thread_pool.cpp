98f1767e8533a7695ae38726213e28c70bb429c0
zscey <zscey.haol@gmail.com>
2026-04-04 23:53:33 +0800

    feat: Implement thread pool

---
+++ tests/thread_pool/test_thread_pool.cpp
@@ -0,0 +1,32 @@
+#include "tiny_llm/thread_pool/thread_pool.hpp"
+#include "gtest/gtest.h"
+#include <numeric>
+
+namespace tiny_llm {
+namespace {
+auto sum(const std::vector<size_t> &vec, size_t beg, size_t end) -> size_t {
+  size_t res{};
+  while (beg < end) {
+    res += vec.at(beg);
+    ++beg;
+  }
+  return res;
+}
+} // namespace
+
+TEST(ThreadPool, ThreadPool) {
+  ThreadPool pool;
+  std::vector<size_t> vec(1000);
+  std::iota(vec.begin(), vec.end(), 0);
+
+  std::vector<std::future<size_t>> futures;
+  futures.reserve(10);
+  for (size_t i = 0; i < 10; ++i) {
+    futures.emplace_back(pool.enqueue(sum, vec, i * 100, (i + 1) * 100));
+  }
+
+  for (size_t i = 0; i < 10; ++i) {
+    EXPECT_TRUE(futures.at(i).get() == (i * 200 + 99) * 50);
+  }
+}
+} // namespace tiny_llm
---
+++ tiny_llm/thread_pool/thread_pool.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include "tiny_llm/common/log_and_excepts.hpp"
+#include <atomic>
+#include <condition_variable>
+#include <deque>
+#include <functional>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <type_traits>
+
+namespace tiny_llm {
+class ThreadPool {
+  struct alignas(64) WorkerContext {
+    std::deque<std::function<void()>> local_queue;
+    std::mutex mtx;
+    std::condition_variable cv;
+  };
+
+public:
+  explicit ThreadPool(
+      size_t threads = std::max(1U, std::thread::hardware_concurrency() - 1)) {
+    workers_.reserve(threads);
+    contexts_.reserve(threads);
+
+    for (size_t i = 0; i < threads; ++i) {
+      auto &ctx = contexts_.emplace_back(std::make_unique<WorkerContext>());
+
+      workers_.emplace_back([&ctx](std::stop_token st) -> void {
+        while (true) {
+          std::function<void()> task;
+          {
+            std::unique_lock lock(ctx->mtx);
+            ctx->cv.wait(lock, [&]() -> bool {
+              return st.stop_requested() || !ctx->local_queue.empty();
+            });
+
+            if (st.stop_requested() && ctx->local_queue.empty()) {
+              break;
+            }
+
+            if (ctx->local_queue.empty()) {
+              continue;
+            }
+
+            task = std::move(ctx->local_queue.front());
+            ctx->local_queue.pop_front();
+          }
+          task();
+        }
+      });
+    }
+  }
+
+  ~ThreadPool() {
+    shutting_down_.store(true, std::memory_order_release);
+
+    for (auto &w : workers_) {
+      w.request_stop();
+    }
+
+    for (auto &ctx : contexts_) {
+      ctx->cv.notify_all();
+    }
+  }
+
+  template <typename F, typename... Args>
+    requires std::invocable<F, Args...>
+  auto enqueue(F &&f, Args &&...args)
+      -> std::future<std::invoke_result_t<F, Args...>> {
+    if (shutting_down_.load(std::memory_order_acquire)) {
+      TINY_LLM_THROW_ERROR(std::runtime_error,
+                           "Enqueue on stopped ThreadPool.");
+    }
+
+    auto task = std::make_shared<
+        std::packaged_task<std::invoke_result_t<F, Args...>()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    size_t idx =
+        next_worker_.fetch_add(1, std::memory_order_relaxed) % contexts_.size();
+    {
+      std::lock_guard lock(contexts_.at(idx)->mtx);
+      contexts_.at(idx)->local_queue.emplace_back(
+          [task]() -> auto { (*task)(); });
+    }
+    contexts_.at(idx)->cv.notify_one();
+
+    return task->get_future();
+  }
+
+  ThreadPool(const ThreadPool &) = delete;
+  auto operator=(const ThreadPool &) -> ThreadPool & = delete;
+  ThreadPool(ThreadPool &&) = delete;
+  auto operator=(ThreadPool &&) -> ThreadPool & = delete;
+
+private:
+  std::vector<std::unique_ptr<WorkerContext>> contexts_;
+  std::atomic<size_t> next_worker_{0};
+  std::atomic<bool> shutting_down_{false};
+
+  // declare `workers_` at the end to support drain policy
+  std::vector<std::jthread> workers_;
+};
+} // namespace tiny_llm
