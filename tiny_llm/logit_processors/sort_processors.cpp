83fab05bf1dd6d57de63dc81b4c3c19953b53d6a
zscey <zscey.haol@gmail.com>
2026-04-06 03:02:48 +0800

    feat: Implement topp processor (#49)
    * feat: Implement topp processor
    
    * test: Supplyment test cases

--- tiny_llm/logit_processors/sort_processors.cpp
+++ tiny_llm/logit_processors/sort_processors.cpp
@@ -6,14 +6,14 @@ namespace tiny_llm {
 namespace {
 // [left, right]
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-auto partition(float *logit, uint32_t *id, uint32_t left, uint32_t right,
-               uint32_t pivot_index) -> uint32_t {
+auto partition(float *logit, uint32_t *id, int32_t left, int32_t right,
+               int32_t pivot_index) -> int32_t {
   float pivot_value = logit[pivot_index];
   std::swap(logit[pivot_index], logit[right]);
   std::swap(id[pivot_index], id[right]);
 
-  uint32_t store_index = left;
-  for (uint32_t i = left; i < right; ++i) {
+  int32_t store_index = left;
+  for (int32_t i = left; i < right; ++i) {
     if (logit[i] > pivot_value) {
       std::swap(logit[store_index], logit[i]);
       std::swap(id[store_index], id[i]);
@@ -29,8 +29,8 @@ auto partition(float *logit, uint32_t *id, uint32_t left, uint32_t right,
 
 // [left, right]
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters, misc-no-recursion)
-void quick_select(float *logit, uint32_t *id, uint32_t left, uint32_t right,
-                  uint32_t k) {
+void quick_select(float *logit, uint32_t *id, int32_t left, int32_t right,
+                  int32_t k) {
   if (left >= right) {
     return;
   }
@@ -50,27 +50,103 @@ void quick_select(float *logit, uint32_t *id, uint32_t left, uint32_t right,
 }
 } // namespace
 
-auto TopKProcessor::apply(Tensor &logit, Tensor &id, uint32_t valid_size) const
-    -> uint32_t {
+void TopKProcessor::apply(Tensor &logit, Tensor &id, Tensor &valid_size) const {
   TINY_LLM_CHECK(logit.dtype() == DataType::kFloat32);
   TINY_LLM_CHECK(id.dtype() == DataType::kUint32);
+  TINY_LLM_CHECK(valid_size.dtype() == DataType::kUint32);
   TINY_LLM_CHECK(logit.shape().size() == 2);
   TINY_LLM_CHECK(logit.shape() == id.shape());
-  TINY_LLM_CHECK(logit.shape().back() >= valid_size);
-  TINY_LLM_CHECK(valid_size >= top_k_);
+
+  int64_t batch = logit.shape().at(0);
+  int64_t dim = logit.shape().at(1);
+  TINY_LLM_CHECK(valid_size.shape() == std::vector<int64_t>{logit.shape()[0]})
+  for (int64_t i = 0; i < batch; ++i) {
+    TINY_LLM_CHECK(valid_size.data<uint32_t>()[i] <= dim);
+    TINY_LLM_CHECK(valid_size.data<uint32_t>()[i] >= top_k_);
+  }
 
   if (top_k_ == 0) {
-    return 0;
+    for (int64_t i = 0; i < batch; ++i) {
+      valid_size.data<uint32_t>()[i] = top_k_;
+    }
+    return;
   }
 
-  for (int64_t b = 0, b_end = logit.shape().at(0); b < b_end; ++b) {
-    auto shift = b * logit.shape().at(1);
+  for (int64_t i = 0; i < batch; ++i) {
+    auto shift = i * dim;
     quick_select(logit.data<float>() + shift, id.data<uint32_t>() + shift, 0,
-                 valid_size - 1, top_k_);
+                 static_cast<int32_t>(valid_size.data<uint32_t>()[i]) - 1,
+                 static_cast<int32_t>(top_k_));
+    valid_size.data<uint32_t>()[i] = top_k_;
   }
-
-  return top_k_;
 }
 
 static_assert(LogitProcessor<TopKProcessor>);
+
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+TopPProcessor::TopPProcessor(float top_p, uint32_t min_tokens_to_keep) {
+  TINY_LLM_CHECK(top_p >= 0.F);
+  TINY_LLM_CHECK(top_p <= 1.F);
+  TINY_LLM_CHECK(min_tokens_to_keep > 0);
+
+  top_p_ = top_p;
+  min_tokens_to_keep_ = min_tokens_to_keep;
+}
+
+namespace {
+// [left, right]
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters,misc-no-recursion)
+void quick_sort(float *logit, uint32_t *id, int32_t left, int32_t right) {
+  if (left >= right) {
+    return;
+  }
+
+  auto mid = left + ((right - left) / 2);
+  auto pivot_index = partition(logit, id, left, right, mid);
+  quick_sort(logit, id, left, pivot_index - 1);
+  quick_sort(logit, id, pivot_index + 1, right);
+}
+
+} // namespace
+
+void TopPProcessor::apply(Tensor &logit, Tensor &id, Tensor &valid_size) const {
+  TINY_LLM_CHECK(logit.dtype() == DataType::kFloat32);
+  TINY_LLM_CHECK(id.dtype() == DataType::kUint32);
+  TINY_LLM_CHECK(valid_size.dtype() == DataType::kUint32);
+  TINY_LLM_CHECK(logit.shape().size() == 2);
+  TINY_LLM_CHECK(logit.shape() == id.shape());
+
+  int64_t batch = logit.shape().at(0);
+  int64_t dim = logit.shape().at(1);
+  TINY_LLM_CHECK(valid_size.shape() == std::vector<int64_t>{logit.shape()[0]})
+  for (int64_t i = 0; i < batch; ++i) {
+    TINY_LLM_CHECK(valid_size.data<uint32_t>()[i] <= dim);
+    TINY_LLM_CHECK(valid_size.data<uint32_t>()[i] >= min_tokens_to_keep_);
+  }
+
+  for (int64_t b = 0; b < batch; ++b) {
+    auto shift = b * dim;
+    auto *logit_ptr = logit.data<float>() + shift;
+    auto logit_size = valid_size.data<uint32_t>()[b];
+    quick_sort(logit_ptr, id.data<uint32_t>() + shift, 0,
+               static_cast<int32_t>(logit_size) - 1);
+
+    std::vector<float> tmp(logit_size);
+    float exp_sum{};
+    for (uint32_t i = 0; i < logit_size; ++i) {
+      tmp.at(i) = std::exp(logit_ptr[i] - logit_ptr[0]);
+      exp_sum += tmp.at(i);
+    }
+
+    auto remain_p = 1 - top_p_;
+    auto valid_idx = logit_size - 1;
+    for (; valid_idx >= min_tokens_to_keep_; --valid_idx) {
+      remain_p -= tmp.at(valid_idx) / exp_sum;
+      if (remain_p < 0) {
+        break;
+      }
+    }
+    valid_size.data<uint32_t>()[b] = valid_idx + 1;
+  }
+}
 } // namespace tiny_llm
