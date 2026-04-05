eefe996b506c2f55ac24e2be85ba89d1eb2803d3
zscey <zscey.haol@gmail.com>
2026-04-05 15:36:07 +0800

    feat: Implement arithmetic(mul) in avx2

--- tiny_llm/ops/cpu/arithmetic.cpp
+++ tiny_llm/ops/cpu/arithmetic.cpp
@@ -1,29 +1,54 @@
 #include "tiny_llm/ops/cpu/arithmetic.hpp"
+#ifdef TINY_LLM_CPU_OPS_WITH_AVX2
+#include <immintrin.h>
+#endif
 
 namespace tiny_llm::cpu {
 namespace {
+namespace avx2 {
+#ifdef TINY_LLM_CPU_OPS_WITH_AVX2
+auto arithmetic_mul_avx2_kernel(const float *left, float right, float *dst,
+                                size_t beg, size_t end) -> size_t {
+  auto right_vec = _mm256_set1_ps(right);
+
+  auto vec_num = (end - beg) / 8 * 8;
+  for (end = beg + vec_num; beg < end; beg += 8) {
+    auto left_vec = _mm256_loadu_ps(left + beg);
+    // NOLINTNEXTLINE(portability-simd-intrinsics)
+    auto res = _mm256_mul_ps(left_vec, right_vec);
+    _mm256_storeu_ps(dst + beg, res);
+  }
+
+  return vec_num;
+}
+#else
+constexpr auto arithmetic_mul_avx2_kernel(const float * /*left*/,
+                                          float /*right*/, float * /*dst*/,
+                                          size_t /*beg*/, size_t /*end*/)
+    -> size_t {
+  return 0;
+}
+#endif
+} // namespace avx2
+
 template <ArithmeticType T>
 void arithmetic_kernel(const float *left, float right, float *dst, size_t beg,
                        size_t end) {
+  static_assert(T == ArithmeticType::kAdd || T == ArithmeticType::kMul ||
+                T == ArithmeticType::kSub);
+
   if constexpr (T == ArithmeticType::kAdd) {
-    while (beg < end) {
+    for (; beg < end; ++beg) {
       dst[beg] = left[beg] + right;
-      ++beg;
     }
   } else if constexpr (T == ArithmeticType::kMul) {
-    while (beg < end) {
+    for (beg += avx2::arithmetic_mul_avx2_kernel(left, right, dst, beg, end);
+         beg < end; ++beg) {
       dst[beg] = left[beg] * right;
-      ++beg;
     }
   } else if constexpr (T == ArithmeticType::kSub) {
-    while (beg < end) {
+    for (; beg < end; ++beg) {
       dst[beg] = left[beg] - right;
-      ++beg;
-    }
-  } else if constexpr (T == ArithmeticType::kDiv) {
-    while (beg < end) {
-      dst[beg] = left[beg] / right;
-      ++beg;
     }
   }
 }
@@ -45,7 +70,15 @@ void arithmetic(const float *left, float right, float *dst, size_t element_size,
     CASE_OF_TYPE(ArithmeticType::kAdd);
     CASE_OF_TYPE(ArithmeticType::kMul);
     CASE_OF_TYPE(ArithmeticType::kSub);
-    CASE_OF_TYPE(ArithmeticType::kDiv);
+  case ArithmeticType::kDiv:
+    TINY_LLM_CHECK(right != 0.F);
+    right = 1.F / right;
+    parallel_run(
+        [left, right, dst](size_t beg, size_t end) -> void {
+          arithmetic_kernel<ArithmeticType::kMul>(left, right, dst, beg, end);
+        },
+        0, element_size);
+    return;
   }
 }
 } // namespace tiny_llm::cpu
