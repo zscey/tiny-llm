6da7a4bb270618b51f801794f2d4ffb28814dcd2
zscey <zscey.haol@gmail.com>
2026-05-17 21:45:37 +0800

    feat: Implement paged rope (#72)

--- tests/ops/cuda/test_rope.cpp
+++ tests/ops/cuda/test_rope.cpp
@@ -96,6 +96,121 @@ TEST(CudaOps, Rope) {
       EXPECT_TRUE(std::abs(dst_ptr[i] - target_ptr[i]) < 1e-4F);
     }
   }
+
+  { // Paged Rope
+    int64_t total_queries = 6;
+    int64_t h = 4;
+    int64_t d = 96;
+
+    Tensor position_ids({.type = DeviceType::kCpu, .id = 0}, DataType::kUint32,
+                        {total_queries});
+    {
+      auto *position_ids_ptr = position_ids.data<uint32_t>();
+      for (uint32_t i = 0; i < 6; ++i) {
+        position_ids_ptr[i] = i & 1U;
+      }
+      position_ids = position_ids.to({.type = DeviceType::kCuda, .id = 0});
+    }
+
+    Tensor dst({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {2, h, 32, d});
+    // 6 queries split to [2, 2, 2]
+    Tensor block_table({.type = DeviceType::kCpu}, DataType::kUint32, {3, 1});
+    {
+      auto *block_table_ptr = block_table.data<uint32_t>();
+      block_table_ptr[0] = 0;
+      block_table_ptr[1] = 1;
+      block_table_ptr[2] = 1;
+      block_table = block_table.to({.type = DeviceType::kCuda, .id = 0});
+    }
+    Tensor seq_separator({.type = DeviceType::kCpu}, DataType::kUint32, {3});
+    {
+      auto *seq_separator_ptr = seq_separator.data<uint32_t>();
+      seq_separator_ptr[0] = 0;
+      seq_separator_ptr[1] = 2;
+      seq_separator_ptr[2] = 4;
+      seq_separator = seq_separator.to({.type = DeviceType::kCuda, .id = 0});
+    }
+    Tensor cache_offsets({.type = DeviceType::kCpu}, DataType::kUint32, {3});
+    {
+      auto *cache_offsets_ptr = cache_offsets.data<uint32_t>();
+      cache_offsets_ptr[0] = 10;
+      cache_offsets_ptr[1] = 15;
+      cache_offsets_ptr[2] = 20;
+      cache_offsets = cache_offsets.to({.type = DeviceType::kCuda, .id = 0});
+    }
+
+    {
+      size_t i{};
+      auto *dst_ptr = dst.data<float>();
+
+      for (int64_t cur_h = 0; cur_h < h; ++cur_h) {
+        auto *cur_ptr = dst_ptr + (((cur_h * 32) + 10) * d);
+        auto *cur_ptr_end = cur_ptr + (2 * d);
+        while (cur_ptr < cur_ptr_end) {
+          *cur_ptr++ = static_cast<float>(i % 100) / 100.F;
+          ++i;
+        }
+      }
+
+      for (int64_t cur_h = 0; cur_h < h; ++cur_h) {
+        auto *cur_ptr = dst_ptr + ((((h + cur_h) * 32) + 15) * d);
+        auto *cur_ptr_end = cur_ptr + (2 * d);
+        while (cur_ptr < cur_ptr_end) {
+          *cur_ptr++ = static_cast<float>(i % 100) / 100.F;
+          ++i;
+        }
+      }
+
+      for (int64_t cur_h = 0; cur_h < h; ++cur_h) {
+        auto *cur_ptr = dst_ptr + ((((h + cur_h) * 32) + 20) * d);
+        auto *cur_ptr_end = cur_ptr + (2 * d);
+        while (cur_ptr < cur_ptr_end) {
+          *cur_ptr++ = static_cast<float>(i % 100) / 100.F;
+          ++i;
+        }
+      }
+
+      dst = dst.to({.type = DeviceType::kCuda, .id = 0});
+    }
+
+    cuda::apply_rope_inplace_paged(
+        cos.data<float>(), sin.data<float>(), position_ids.data<uint32_t>(),
+        dst.data<float>(), block_table.data<uint32_t>(),
+        seq_separator.data<uint32_t>(), cache_offsets.data<uint32_t>(), 6, 3, 4,
+        96, 1, 32);
+    dst = dst.to({.type = DeviceType::kCpu, .id = 0});
+    ThreadCudaContexts::Synchronize();
+
+    const auto *dst_ptr = dst.data<float>();
+    SafeTensorWeightManager wm(utils::BazelRunfile::RLocation(
+        "tiny_llm/tests/datas/apply_rope_b3_h4_q2_d96_qs1qe5.safetensors"));
+    const auto *target_ptr =
+        reinterpret_cast<const float *>(wm.get_tensor("res").data);
+    for (int64_t cur_h = 0; cur_h < h; ++cur_h) {
+      {
+        const auto *cur_dst_ptr = dst_ptr + (((cur_h * 32) + 10) * d);
+        const auto *cur_target_ptr = target_ptr + ((cur_h * 5 + 1) * d);
+        for (size_t i = 0; i < 192; ++i) {
+          EXPECT_TRUE(std::abs(cur_dst_ptr[i] - cur_target_ptr[i]) < 1e-4F);
+        }
+      }
+      {
+        const auto *cur_dst_ptr = dst_ptr + ((((4 + cur_h) * 32) + 15) * d);
+        const auto *cur_target_ptr = target_ptr + (((4 + cur_h) * 5 + 1) * d);
+        for (size_t i = 0; i < 192; ++i) {
+          EXPECT_TRUE(std::abs(cur_dst_ptr[i] - cur_target_ptr[i]) < 1e-4F);
+        }
+      }
+      {
+        const auto *cur_dst_ptr = dst_ptr + ((((4 + cur_h) * 32) + 20) * d);
+        const auto *cur_target_ptr = target_ptr + (((8 + cur_h) * 5 + 1) * d);
+        for (size_t i = 0; i < 192; ++i) {
+          EXPECT_TRUE(std::abs(cur_dst_ptr[i] - cur_target_ptr[i]) < 1e-4F);
+        }
+      }
+    }
+  }
 }
 
 } // namespace tiny_llm
--- tiny_llm/ops/cuda/rope.cu
+++ tiny_llm/ops/cuda/rope.cu
@@ -2,9 +2,11 @@
 #include "tiny_llm/common/checks.hpp"
 #include "tiny_llm/common/exception.hpp"
 #include "tiny_llm/ops/cuda/rope.hpp"
+#include "tiny_llm/ops/details.hpp"
 
 namespace tiny_llm::cuda {
 namespace {
+constexpr uint32_t kPageSize = 32;
 constexpr uint32_t kThreadNum = 512;
 
 __global__ void rope_kernel(float *cos_dst, float *sin_dst, uint32_t max_len,
@@ -52,6 +54,45 @@ __global__ void apply_rope_inplace_kernel(const float *cos, const float *sin,
     }
   }
 }
+
+__global__ void apply_rope_inplace_paged_kernel(
+    const float *cos, const float *sin, const uint32_t *position_ids,
+    float *page_pool, const uint32_t *block_table,
+    const uint32_t *seq_separator, const uint32_t *cache_offsets,
+    uint32_t total_queries, uint32_t num_requests, uint32_t num_head,
+    uint32_t half_dim, uint32_t max_blocks) {
+  auto t_shift = (static_cast<size_t>(blockIdx.x) * blockDim.x) + threadIdx.x;
+  auto query_idx = static_cast<uint32_t>(t_shift / half_dim);
+  if (query_idx < total_queries) {
+    auto dim_idx = t_shift % half_dim;
+    auto pe_shift =
+        (static_cast<size_t>(position_ids[query_idx]) * half_dim) + dim_idx;
+    auto cos_factor = cos[pe_shift];
+    auto sin_factor = sin[pe_shift];
+
+    auto request_idx =
+        detail::get_request_idx(query_idx, seq_separator, 0, num_requests);
+    auto relative_row =
+        query_idx - seq_separator[request_idx] + cache_offsets[request_idx];
+    auto page_idx =
+        block_table[(request_idx * max_blocks) + (relative_row / kPageSize)];
+    auto token_idx = relative_row % kPageSize;
+    auto *real_ptr =
+        page_pool +
+        ((static_cast<size_t>(page_idx) * num_head * kPageSize + token_idx) *
+         half_dim * 2) +
+        dim_idx;
+    for (uint32_t i = 0; i < num_head; ++i) {
+      auto *imaginary_ptr = real_ptr + half_dim;
+      auto real = *real_ptr;
+      auto imaginary = *imaginary_ptr;
+      *real_ptr = (cos_factor * real) - (sin_factor * imaginary);
+      *imaginary_ptr = (sin_factor * real) + (cos_factor * imaginary);
+
+      real_ptr += static_cast<size_t>(kPageSize) * half_dim * 2;
+    }
+  }
+}
 } // namespace
 
 void rope(float *cos_dst, float *sin_dst, uint32_t max_len, uint32_t dim,
@@ -85,5 +126,30 @@ void apply_rope_inplace(const float *cos, const float *sin,
                               ThreadCudaContexts::GetContext().stream>>>(
       cos, sin, position_ids, dst, element_size, head_num, seq_len, half_dim,
       seq_start, seq_end);
+}
+
+void apply_rope_inplace_paged(const float *cos, const float *sin,
+                              const uint32_t *position_ids, float *page_pool,
+                              const uint32_t *block_table,
+                              const uint32_t *seq_separator,
+                              const uint32_t *cache_offsets,
+                              uint32_t total_queries, uint32_t num_requests,
+                              uint32_t num_head, uint32_t head_dim,
+                              uint32_t max_blocks, uint32_t page_size) {
+  TINY_LLM_CHECK(InvalidArgumentError, total_queries > 0);
+  TINY_LLM_CHECK(InvalidArgumentError, num_requests > 0);
+  TINY_LLM_CHECK(InvalidArgumentError, num_head > 0);
+  TINY_LLM_CHECK(InvalidArgumentError, head_dim > 0);
+  TINY_LLM_CHECK(InvalidArgumentError, (head_dim & 1U) == 0);
+  TINY_LLM_CHECK(InvalidArgumentError, max_blocks > 0);
+  TINY_LLM_CHECK(InvalidArgumentError, page_size == kPageSize);
+
+  auto half_dim = head_dim / 2;
+  apply_rope_inplace_paged_kernel<<<
+      CalBlockNum(total_queries * half_dim, kThreadNum), kThreadNum, 0,
+      ThreadCudaContexts::GetContext().stream>>>(
+      cos, sin, position_ids, page_pool, block_table, seq_separator,
+      cache_offsets, total_queries, num_requests, num_head, half_dim,
+      max_blocks);
 }
 } // namespace tiny_llm::cuda
