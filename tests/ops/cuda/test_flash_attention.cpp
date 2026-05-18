e666798569f4ab16e6379ef6f32ad73823e5a8f4
zscey <zscey.haol@gmail.com>
2026-05-18 20:26:07 +0800

    feat: Implement paged attention

--- tests/ops/cuda/test_flash_attention.cpp
+++ tests/ops/cuda/test_flash_attention.cpp
@@ -21,7 +21,7 @@ TEST(CudaOps, FlashAttnNoMask) {
   for (size_t i = 0,
               i_end = static_cast<size_t>(batch) * q_head * q_length * dim;
        i < i_end; ++i) {
-    query_ptr[i] = static_cast<float>(i % 100) / 100;
+    query_ptr[i] = static_cast<float>(i % 100) / 100.F;
   }
   query = query.to({.type = DeviceType::kCuda, .id = 0});
 
@@ -36,7 +36,7 @@ TEST(CudaOps, FlashAttnNoMask) {
             key_ptr + ((((b * kv_head) + h) * (kv_length + kv_padding)) * dim);
         for (size_t shift = 0, shift_end = static_cast<size_t>(kv_length) * dim;
              shift < shift_end; ++shift) {
-          cur_ptr[shift] = static_cast<float>((i + 1) % 100) / 100;
+          cur_ptr[shift] = static_cast<float>((i + 1) % 100) / 100.F;
           ++i;
         }
       }
@@ -56,7 +56,7 @@ TEST(CudaOps, FlashAttnNoMask) {
             ((((b * kv_head) + h) * (kv_length + kv_padding)) * dim);
         for (size_t shift = 0, shift_end = static_cast<size_t>(kv_length) * dim;
              shift < shift_end; ++shift) {
-          cur_ptr[shift] = static_cast<float>((i + 2) % 100) / 100;
+          cur_ptr[shift] = static_cast<float>((i + 2) % 100) / 100.F;
           ++i;
         }
       }
@@ -103,7 +103,7 @@ TEST(CudaOps, FlashAttnCausalMask) {
   for (size_t i = 0,
               i_end = static_cast<size_t>(batch) * q_head * q_length * dim;
        i < i_end; ++i) {
-    query_ptr[i] = static_cast<float>(i % 100) / 100;
+    query_ptr[i] = static_cast<float>(i % 100) / 100.F;
   }
   query = query.to({.type = DeviceType::kCuda, .id = 0});
 
@@ -118,7 +118,7 @@ TEST(CudaOps, FlashAttnCausalMask) {
             key_ptr + ((((b * kv_head) + h) * (kv_length + kv_padding)) * dim);
         for (size_t shift = 0, shift_end = static_cast<size_t>(kv_length) * dim;
              shift < shift_end; ++shift) {
-          cur_ptr[shift] = static_cast<float>((i + 1) % 100) / 100;
+          cur_ptr[shift] = static_cast<float>((i + 1) % 100) / 100.F;
           ++i;
         }
       }
@@ -138,7 +138,7 @@ TEST(CudaOps, FlashAttnCausalMask) {
             ((((b * kv_head) + h) * (kv_length + kv_padding)) * dim);
         for (size_t shift = 0, shift_end = static_cast<size_t>(kv_length) * dim;
              shift < shift_end; ++shift) {
-          cur_ptr[shift] = static_cast<float>((i + 2) % 100) / 100;
+          cur_ptr[shift] = static_cast<float>((i + 2) % 100) / 100.F;
           ++i;
         }
       }
@@ -165,6 +165,209 @@ TEST(CudaOps, FlashAttnCausalMask) {
                 i_end = static_cast<size_t>(batch) * q_length * q_head * dim;
          i < i_end; ++i) {
       EXPECT_TRUE(std::abs(dst_ptr[i] - target_ptr[i]) < 1e-5F);
+    }
+  }
+}
+
+TEST(CudaOps, FlashAttnNoMaskPaged) {
+  int64_t batch = 2;
+  int64_t q_length = 107;
+  int64_t q_head = 4;
+  int64_t kv_length = 123;
+  int64_t kv_head = 2;
+  int64_t dim = 61;
+
+  Tensor query({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {q_head, batch * q_length, dim});
+  {
+    auto *query_ptr = query.data<float>();
+    size_t i{};
+    for (int64_t b = 0; b < batch; ++b) {
+      for (int64_t h = 0; h < q_head; ++h) {
+        auto *cur_ptr = query_ptr + (((h * batch) + b) * q_length * dim);
+        for (int64_t qd = 0, qd_end = q_length * dim; qd < qd_end; ++qd) {
+          cur_ptr[qd] = static_cast<float>(i % 100) / 100.F;
+          ++i;
+        }
+      }
+    }
+    query = query.to({.type = DeviceType::kCuda, .id = 0});
+  }
+
+  Tensor key({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+             {8, kv_head, 32, dim});
+  auto *key_ptr = key.data<float>();
+  Tensor value({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {8, kv_head, 32, dim});
+  auto *value_ptr = value.data<float>();
+  {
+    size_t i{};
+    for (int64_t b = 0; b < batch; ++b) {
+      for (int64_t h = 0; h < kv_head; ++h) {
+        for (int64_t q = 0; q < kv_length; ++q) {
+          auto shift = (((b * 4 + q / 32) * kv_head + h) * 32 + (q % 32)) * dim;
+          auto *cur_key_ptr = key_ptr + shift;
+          auto *cur_value_ptr = value_ptr + shift;
+          for (int64_t d = 0; d < dim; ++d) {
+            cur_key_ptr[d] = static_cast<float>((i + 1) % 100) / 100.F;
+            cur_value_ptr[d] = static_cast<float>((i + 2) % 100) / 100.F;
+            ++i;
+          }
+        }
+      }
+    }
+    key = key.to({.type = DeviceType::kCuda, .id = 0});
+    value = value.to({.type = DeviceType::kCuda, .id = 0});
+  }
+
+  Tensor dst({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+             {q_head, batch * q_length, dim});
+  Tensor q_separator({.type = DeviceType::kCpu}, DataType::kUint32, {3});
+  {
+    auto *q_separator_ptr = q_separator.data<uint32_t>();
+    q_separator_ptr[0] = 0;
+    q_separator_ptr[1] = 107;
+    q_separator_ptr[2] = 214;
+    q_separator = q_separator.to({.type = DeviceType::kCuda, .id = 0});
+  }
+  Tensor block_table({.type = DeviceType::kCpu}, DataType::kUint32, {2, 4});
+  {
+    auto *block_table_ptr = block_table.data<uint32_t>();
+    for (uint32_t i = 0; i < 8; ++i) {
+      block_table_ptr[i] = i;
+    }
+    block_table = block_table.to({.type = DeviceType::kCuda, .id = 0});
+  }
+  Tensor kv_lens({.type = DeviceType::kCpu}, DataType::kUint32, {2});
+  {
+    auto *kv_lens_ptr = kv_lens.data<uint32_t>();
+    kv_lens_ptr[0] = 123;
+    kv_lens_ptr[1] = 123;
+    kv_lens = kv_lens.to({.type = DeviceType::kCuda, .id = 0});
+  }
+  cuda::flash_attn_paged(query.data<float>(), key.data<float>(),
+                         value.data<float>(), dst.data<float>(),
+                         q_separator.data<uint32_t>(),
+                         block_table.data<uint32_t>(), kv_lens.data<uint32_t>(),
+                         2, 4, 2, 61, 4, 32, 107, cuda::AttentionType::kNoMask);
+  dst = dst.to({.type = DeviceType::kCpu, .id = 0});
+  ThreadCudaContexts::Synchronize();
+
+  SafeTensorWeightManager wm(utils::BazelRunfile::RLocation(
+      "tiny_llm/tests/datas/attn_res_b2q107h4k123h2d61.safetensors"));
+  const auto *dst_ptr = dst.data<float>();
+  const auto *target_ptr =
+      reinterpret_cast<const float *>(wm.get_tensor("res").data);
+
+  for (int64_t b = 0; b < batch; ++b) {
+    for (int64_t h = 0; h < q_head; ++h) {
+      const auto *cur_ptr = dst_ptr + (((h * batch) + b) * q_length * dim);
+      for (int64_t qd = 0, qd_end = q_length * dim; qd < qd_end; ++qd) {
+        EXPECT_TRUE(std::abs(cur_ptr[qd] - *target_ptr) < 1e-5F);
+        ++target_ptr;
+      }
+    }
+  }
+}
+
+TEST(CudaOps, FlashAttnCausalMaskPaged) {
+  int64_t batch = 2;
+  int64_t q_length = 107;
+  int64_t q_head = 4;
+  int64_t kv_length = q_length;
+  int64_t kv_head = 2;
+  int64_t dim = 61;
+
+  Tensor query({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {q_head, batch * q_length, dim});
+  {
+    auto *query_ptr = query.data<float>();
+    size_t i{};
+    for (int64_t b = 0; b < batch; ++b) {
+      for (int64_t h = 0; h < q_head; ++h) {
+        auto *cur_ptr = query_ptr + (((h * batch) + b) * q_length * dim);
+        for (int64_t qd = 0, qd_end = q_length * dim; qd < qd_end; ++qd) {
+          cur_ptr[qd] = static_cast<float>(i % 100) / 100.F;
+          ++i;
+        }
+      }
+    }
+    query = query.to({.type = DeviceType::kCuda, .id = 0});
+  }
+
+  Tensor key({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+             {8, kv_head, 32, dim});
+  auto *key_ptr = key.data<float>();
+  Tensor value({.type = DeviceType::kCpu, .id = 0}, DataType::kFloat32,
+               {8, kv_head, 32, dim});
+  auto *value_ptr = value.data<float>();
+  {
+    size_t i{};
+    for (int64_t b = 0; b < batch; ++b) {
+      for (int64_t h = 0; h < kv_head; ++h) {
+        for (int64_t q = 0; q < kv_length; ++q) {
+          auto shift = (((b * 4 + q / 32) * kv_head + h) * 32 + (q % 32)) * dim;
+          auto *cur_key_ptr = key_ptr + shift;
+          auto *cur_value_ptr = value_ptr + shift;
+          for (int64_t d = 0; d < dim; ++d) {
+            cur_key_ptr[d] = static_cast<float>((i + 1) % 100) / 100.F;
+            cur_value_ptr[d] = static_cast<float>((i + 2) % 100) / 100.F;
+            ++i;
+          }
+        }
+      }
+    }
+    key = key.to({.type = DeviceType::kCuda, .id = 0});
+    value = value.to({.type = DeviceType::kCuda, .id = 0});
+  }
+
+  Tensor dst({.type = DeviceType::kCuda, .id = 0}, DataType::kFloat32,
+             {q_head, batch * q_length, dim});
+  Tensor q_separator({.type = DeviceType::kCpu}, DataType::kUint32, {3});
+  {
+    auto *q_separator_ptr = q_separator.data<uint32_t>();
+    q_separator_ptr[0] = 0;
+    q_separator_ptr[1] = 107;
+    q_separator_ptr[2] = 214;
+    q_separator = q_separator.to({.type = DeviceType::kCuda, .id = 0});
+  }
+  Tensor block_table({.type = DeviceType::kCpu}, DataType::kUint32, {2, 4});
+  {
+    auto *block_table_ptr = block_table.data<uint32_t>();
+    for (uint32_t i = 0; i < 8; ++i) {
+      block_table_ptr[i] = i;
+    }
+    block_table = block_table.to({.type = DeviceType::kCuda, .id = 0});
+  }
+  Tensor kv_lens({.type = DeviceType::kCpu}, DataType::kUint32, {2});
+  {
+    auto *kv_lens_ptr = kv_lens.data<uint32_t>();
+    kv_lens_ptr[0] = 107;
+    kv_lens_ptr[1] = 107;
+    kv_lens = kv_lens.to({.type = DeviceType::kCuda, .id = 0});
+  }
+
+  cuda::flash_attn_paged(
+      query.data<float>(), key.data<float>(), value.data<float>(),
+      dst.data<float>(), q_separator.data<uint32_t>(),
+      block_table.data<uint32_t>(), kv_lens.data<uint32_t>(), 2, 4, 2, 61, 4,
+      32, 107, cuda::AttentionType::kCausalMask);
+  dst = dst.to({.type = DeviceType::kCpu, .id = 0});
+  ThreadCudaContexts::Synchronize();
+
+  SafeTensorWeightManager wm(utils::BazelRunfile::RLocation(
+      "tiny_llm/tests/datas/causal_attn_res_b2l107q4k2d61.safetensors"));
+  const auto *dst_ptr = dst.data<float>();
+  const auto *target_ptr =
+      reinterpret_cast<const float *>(wm.get_tensor("res").data);
+
+  for (int64_t b = 0; b < batch; ++b) {
+    for (int64_t h = 0; h < q_head; ++h) {
+      const auto *cur_ptr = dst_ptr + (((h * batch) + b) * q_length * dim);
+      for (int64_t qd = 0, qd_end = q_length * dim; qd < qd_end; ++qd) {
+        EXPECT_TRUE(std::abs(cur_ptr[qd] - *target_ptr) < 1e-5F);
+        ++target_ptr;
+      }
     }
   }
 }
--- tiny_llm/ops/cuda/flash_attention.hpp
+++ tiny_llm/ops/cuda/flash_attention.hpp
@@ -30,4 +30,32 @@ void flash_attn(const float *query, const float *key, const float *value,
                 float *dst, uint32_t batch, uint32_t q_head, uint32_t kv_head,
                 uint32_t q_length, uint32_t kv_length, uint32_t dim,
                 uint32_t kv_end, AttentionType attn_type);
+
+/**
+ * @brief Paged flash attention.
+ *
+ * @param query [q_head, total_queries, head_dim], where
+ * `total_queries=sum(q_lens[i])`
+ * @param key_pool [num_page, kv_head, page_size, head_dim]
+ * @param value_pool [num_page, kv_head, page_size, head_dim]
+ * @param dst [q_head, total_queries, head_dim]
+ * @param q_separator [num_requests + 1]
+ * @param block_table [num_requests, max_blocks]
+ * @param kv_lens [num_requests]
+ * @param num_requests
+ * @param q_head
+ * @param kv_head
+ * @param head_dim
+ * @param max_blocks
+ * @param page_size
+ * @param max_q_len max(q_lens[i])
+ * @param attn_type
+ */
+void flash_attn_paged(const float *query, const float *key_pool,
+                      const float *value_pool, float *dst,
+                      const uint32_t *q_separator, const uint32_t *block_table,
+                      const uint32_t *kv_lens, uint32_t num_requests,
+                      uint32_t q_head, uint32_t kv_head, uint32_t head_dim,
+                      uint32_t max_blocks, uint32_t page_size,
+                      uint32_t max_q_len, AttentionType attn_type);
 } // namespace tiny_llm::cuda
